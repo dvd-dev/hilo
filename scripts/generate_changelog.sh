f32366e67c2ed227203a5569293d6c2f548b3149
David Vallee Delisle <me@dvd.dev>
2021-12-30 12:01:34 -0500

    Reformatting changelog script output

--- scripts/generate_changelog.sh
+++ scripts/generate_changelog.sh
@@ -2,5 +2,5 @@
 git log --oneline | while read l;do
   CID=$(echo $l | cut -f 1 -d " ")
   MSG=$(echo $l | cut -f 2- -d " ")
-  echo "[$MSG](https://github.com/dvd-dev/hilo/commit/$CID)"
+  echo "* [$MSG](https://github.com/dvd-dev/hilo/commit/$CID)"
 done
