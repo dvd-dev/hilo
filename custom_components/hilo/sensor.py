2920bd7a7f5259b3e067a0ca1bd8f1832301bdb7
Ian C. <108159253+ic-dev21@users.noreply.github.com>
2025-01-31 21:15:14 -0500

    Update sensor.py

--- custom_components/hilo/sensor.py
+++ custom_components/hilo/sensor.py
@@ -889,11 +889,6 @@ class HiloChallengeSensorWebsocket(HiloEntity, SensorEntity):
     async def async_added_to_hass(self):
         """Handle entity about to be added to hass event."""
         await super().async_added_to_hass()
-        # last_state = await self.async_get_last_state()
-        # if last_state:
-        #    self._last_update = dt_util.utcnow()
-        #    self._state = last_state.state
-        #    self._next_events = last_state.attributes.get("next_events", [])
 
     async def _async_update(self):
         """This method can be kept for fallback but shouldn't be needed with websockets."""
