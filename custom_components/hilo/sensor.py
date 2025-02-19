a873192bdf2df7e58ad18cc98cf7021e9f001d84
Ian C. <108159253+ic-dev21@users.noreply.github.com>
2025-02-18 20:18:23 -0500

    Revert "Extra state check"
    This reverts commit 11d5af6e4df7f04c1b8671e1744a71ad23f1bf7e.

--- custom_components/hilo/sensor.py
+++ custom_components/hilo/sensor.py
@@ -909,10 +909,9 @@ class HiloChallengeSensor(HiloEntity, SensorEntity):
 
     async def _async_update(self):
         """This method can be kept for fallback but shouldn't be needed with websockets."""
-        if self.state == "reduction":
-            for event_id in self._events:
-                LOG.debug(f"ASYNC UPDATE: EVENT: {event_id}")
-                await self._hilo.request_challenge_consumption_update(1, event_id)
+        for event_id in self._events:
+            LOG.debug(f"ASYNC UPDATE: EVENT: {event_id}")
+            await self._hilo.request_challenge_consumption_update(1, event_id)
 
 
 class DeviceSensor(HiloEntity, SensorEntity):
