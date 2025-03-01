b94b0fcae33298248864a590723e131759996ff5
Ian C. <108159253+ic-dev21@users.noreply.github.com>
2025-02-28 19:10:57 -0500

    Fucked up rebase

--- custom_components/hilo/__init__.py
+++ custom_components/hilo/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import asyncio
 from collections import OrderedDict
 from datetime import datetime, timedelta
+import traceback
 from typing import TYPE_CHECKING, Union
 
 from homeassistant.components.select import (
