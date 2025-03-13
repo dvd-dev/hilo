ad17b7593260ca13b12fd82a9ba3d54dd580a8c6
Christophe Gagnier <Moustachauve@users.noreply.github.com>
2025-03-13 03:17:04 +0000

    Handle the case where the history file is empty

--- custom_components/hilo/sensor.py
+++ custom_components/hilo/sensor.py
@@ -7,6 +7,8 @@ from datetime import datetime, timedelta, timezone
 from os.path import isfile
 
 import aiofiles
+import homeassistant.util.dt as dt_util
+import ruyaml as yaml
 from homeassistant.components.integration.sensor import METHOD_LEFT, IntegrationSensor
 from homeassistant.components.sensor import (
     SensorDeviceClass,
@@ -26,6 +28,8 @@ from homeassistant.const import (
     UnitOfPower,
     UnitOfSoundPressure,
     UnitOfTemperature,
+)
+from homeassistant.const import (
     __short_version__ as current_version,
 )
 from homeassistant.core import HomeAssistant
@@ -34,13 +38,11 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.event import async_track_state_change_event
 from homeassistant.helpers.restore_state import RestoreEntity
 from homeassistant.util import Throttle, slugify
-import homeassistant.util.dt as dt_util
 from packaging.version import Version
 from pyhilo.const import UNMONITORED_DEVICES
 from pyhilo.device import HiloDevice
 from pyhilo.event import Event
 from pyhilo.util import from_utc_timestamp
-import ruyaml as yaml
 
 from . import Hilo, HiloEntity
 from .const import (
@@ -747,6 +749,10 @@ class HiloRewardSensor(HiloEntity, RestoreEntity, SensorEntity):
                 LOG.debug("Loading history state from yaml")
                 content = await yaml_file.read()
                 history = yaml.load(content, Loader=yaml.Loader)
+                if not history:
+                    LOG.warning("History state is corrupted, resetting to default.")
+                    history = []
+
         return history
 
     async def _save_history(self, history: list):
